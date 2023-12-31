15578e6d0e9d633915cf4373270bba4e0b02a7cd
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2023-12-31 18:32:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,41 @@ import {
 } from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login/login";
+import { Signup } from "./pages/signup/signup";
+import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "./firebase.init";
+import { Loader } from "./pages/loader";
+
+const ProtectedRoute = ({ children }) => {
+  const [user, loading] = useAuthState(auth)
+  console.log("user:", user);
+  console.log("loading:", loading);
+
+  if (loading) {
+    return <Loader />
+  }
+  if (!user) {
+    return <Navigate to={'/login'} />
+  }
+  return children;
+}
 
 function App() {
 
   return (
-    <>
+    <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/loader" element={<Loader />} />
           <Route />
           <Route />
         </Routes>
       </Router>
-    </>
+    </div>
   )
 }
 

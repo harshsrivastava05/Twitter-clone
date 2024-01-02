08d724f577c2daae00174dfdbf80e718d8046942
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-02 17:44:27 +0530

    Add files via upload

---
+++ frontend/src/pages/profile/profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="page">
+            <h2 className="pagetitle">this is Profile</h2>
+        </div>
+    )
+}
+
+export default Profile;

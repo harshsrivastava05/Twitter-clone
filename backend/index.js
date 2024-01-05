e1b552dba09cb4ad355f9c38728bd16a4dbe1f1d
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-05 23:33:57 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const url =
+  "mongodb+srv://harshsrivastava07:manya1234harsh@cluster0.bsbrmty.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const postCollection = client.db("Twitter").collection("posts");
+    const userCollection = client.db("Twitter").collection("users");
+
+    app.get("/post", async (req, res) => {
+      const post = await postCollection.find().toArray();
+      res.send(post);
+    });
+
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+
+    app.get("/logedinuser", async (req, res) => {
+      const email = req.query.email;
+      const user = await userCollection.findOne({ email });
+      res.send(user);
+    });
+
+    app.post("/signup", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } catch (error) {
+    console.error("Error saving user:", error);
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});

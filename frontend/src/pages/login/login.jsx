22a7bc9508e125b0cee1066f3b5a77f783255d45
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-05 23:37:11 +0530

    Update login.jsx

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,122 +1,108 @@
 import twittericon from "../../assets/images/twittericon.jpg";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import React, { useState } from "react";
-import { useSignInWithEmailAndPassword,useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from "../../firebase.init"
 import GoogleButton from 'react-google-button'
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.css"
+import { useUserAuth } from "../../context/UserAuthContext";
 
 const Login = () => {
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
-    const [signInWithGoogle, googleuser, googleloading, googleerror] = useSignInWithGoogle(auth);
-    const navigate = useNavigate();
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useSignInWithEmailAndPassword(auth);
-
-    if (user || googleuser) {
-        navigate('/')
-        console.log(user)
-        console.log(googleuser)
-    }
-    if (error || googleerror) {
-        console.log(error.message)
-        console.log(googleerror.message)
-    }
-    if (loading || googleloading) {
-        console.log("loading...")
-    }
-
-    const handleGoogleSignIn = async (e) => {
-        e.preventDefault();
-        signInWithGoogle()
-
-    };
+    const [error, setError] = useState("");
+    const { logIn, googleSignIn } = useUserAuth();
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // setError("");
+        setError("");
         try {
-            await signInWithEmailAndPassword(email, password);
+            await logIn(email, password);
             navigate("/");
         } catch (err) {
+            setError(err.message);
             window.alert(err.message);
+        }
+    };
+
+    const handleGoogleSignIn = async (e) => {
+        e.preventDefault();
+        try {
+            await googleSignIn();
+            navigate("/");
+        } catch (error) {
+            console.log(error.message);
         }
     };
 
     return (
         <div className="login-container">
-        <div className="image-container">
-            <img className=" image" src={twittericon} alt="twitterImage" />
-        </div>
+            <div className="image-container">
+                <img className=" image" src={twittericon} alt="twitterImage" />
+            </div>
 
-        <div className="form-container">
-            <div className="form-box" >
-                <TwitterIcon className="Twitter-img"  />
-                <h2 className="heading">Happening now</h2>
-                <div class="d-flex align-items-sm-center">
+            <div className="form-container">
+                <div className="form-box" >
+                    <TwitterIcon className="Twittericon" />
+                    <h2 className="heading">Happening now</h2>
+                    <div class="d-flex align-items-sm-center">
                         <h3 className="heading1"> Login now! </h3>
                     </div>
 
-                {error && <p>{error.message}</p>}
-                <form onSubmit={handleSubmit}>
+                    {error && <p>{error.message}</p>}
+                    <form onSubmit={handleSubmit}>
 
-                    <input
-                        type="email" className="email"
-                        placeholder="Email address"
-                        onChange={(e) => setemail(e.target.value)}
-                    />
-
-
-
-                    <input className="password"
-                        type="password"
-                        placeholder="Password"
-                        onChange={(e) => setpassword(e.target.value)}
-                    />
+                        <input
+                            type="email" className="email"
+                            placeholder="Email address"
+                            onChange={(e) => setemail(e.target.value)}
+                        />
 
 
-                    <div className="btn-login">
-                        <button type="submit" className="btn" >Log In</button>
+
+                        <input className="password"
+                            type="password"
+                            placeholder="Password"
+                            onChange={(e) => setpassword(e.target.value)}
+                        />
+
+
+                        <div className="btn-login">
+                            <button type="submit" className="btn" >Log In</button>
+                        </div>
+                    </form>
+                    <hr />
+                    <div>
+                        <GoogleButton
+
+                            className="g-btn"
+                            type="light"
+
+                            onClick={handleGoogleSignIn}
+                        />
+
+
                     </div>
-                </form>
-                <hr />
-                <div>
-                    <GoogleButton
-
-                        className="g-btn"
-                        type="light"
-
-                        onClick={handleGoogleSignIn}
-                    />
-
-
                 </div>
+                <div>
+                    Don't have an account?
+                    <Link
+                        to="/signup"
+                        style={{
+                            textDecoration: 'none',
+                            color: 'skyblue',
+                            fontWeight: '600',
+                            marginLeft: '5px'
+                        }}
+                    >
+                        Sign up
+                    </Link>
+                </div>
+
             </div>
-            <div>
-                Don't have an account?
-                <Link
-                    to="/signup"
-                    style={{
-                        textDecoration: 'none',
-                        color: 'skyblue',
-                        fontWeight: '600',
-                        marginLeft: '5px'
-                    }}
-                >
-                    Sign up
-                </Link>
-            </div>
+
 
         </div>
-
-
-    </div>
     )
 }
 

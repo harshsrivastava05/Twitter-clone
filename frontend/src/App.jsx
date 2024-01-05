5d61ced10352f6c8aefe1128a6dcf9d40c0d8076
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-05 23:41:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,8 @@ import Message from "./pages/Messages/message";
 import More from "./pages/more/more";
 import Notification from "./pages/notification/notification";
 import Profile from "./pages/profile/profile";
+import Tweetbox from "./pages/feed/tweetbox/tweetbox";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 
 
@@ -27,24 +29,29 @@ function App() {
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} >
-            <Route path="feed" element={<Feed />}/>
-            <Route path="bookmarks" element={<Bookmark />}/>
-            <Route path="explore" element={<Explore />}/>
-            <Route path="list" element={<List />}/>
-            <Route path="messages" element={<Message />}/>
-            <Route path="more" element={<More />}/>
-            <Route path="notification" element={<Notification />}/>
-            <Route path="profile" element={<Profile />}/>
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/loader" element={<Loader />} />
-          <Route />
-          <Route />
-        </Routes>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>}>
+              <Route index element={<Feed />} />
+            </Route>
+            <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} >
+              <Route path="feed" element={<Feed />} />
+              <Route path="bookmark" element={<Bookmark />} />
+              <Route path="explore" element={<Explore />} />
+              <Route path="list" element={<List />} />
+              <Route path="messages" element={<Message />} />
+              <Route path="more" element={<More />} />
+              <Route path="notification" element={<Notification />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="tweet" element={<Tweetbox />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/loader" element={<Loader />} />
+            <Route />
+            <Route />
+          </Routes>
+        </UserAuthContextProvider>
       </Router>
     </div>
   )

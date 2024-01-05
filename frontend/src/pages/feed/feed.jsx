caf2c08c032c0b798af01dd5ad51b7358c150f7b
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-06 01:26:14 +0530

    Update feed.jsx

--- frontend/src/pages/feed/feed.jsx
+++ frontend/src/pages/feed/feed.jsx
@@ -1,13 +1,33 @@
-import React from "react";
-import './feed.css';
+import React, { useEffect, useState } from "react";
+import Post from "./Post/Post";
+import "./feed.css";
+import Tweetbox from "./tweetbox/tweetbox";
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
 
-    return(
-        <div>
+    useEffect(() => {
+        
+        fetch('http://localhost:3000/post')
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data);
+            })
+    }, [posts])
 
+    return (
+        <div className="feed">
+            <div className="feed__header">
+                <h2 style={{color:'#50b7f5'}}>Home</h2>
+            </div>
+           <Tweetbox />
+            {
+                posts.map(p => <Post key={p._id} p={p} />)
+            }
         </div>
+
     )
 
-};
-export default Feed;
+}
+
+export default Feed

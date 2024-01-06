99e07b0340d6780b864fc6b581525b836b90a73e
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-01-06 22:21:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ async function run() {
     const userCollection = client.db("Twitter").collection("users");
 
     app.get("/post", async (req, res) => {
-      const post = await postCollection.find().toArray();
+      const post = (await postCollection.find().toArray()).reverse();
       res.send(post);
     });
 
@@ -43,6 +43,16 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.patch('/userUpdates/:email', async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = { $set: profile };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+  })
+  
   } catch (error) {
     console.error("Error saving user:", error);
   }

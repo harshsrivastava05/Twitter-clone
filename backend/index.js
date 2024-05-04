85714959c87409fd8cb61c659956df1d194bd33a
harsh srivastava <130855160+harshsrivastava05@users.noreply.github.com>
2024-05-04 14:15:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,16 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({
-  origin: "https://twitter-clone-frontend-gules.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-}));
+app.use(cors());
 app.use(express.json());
 
 const url =
-  "mongodb+srv://harshsrivastava07:manya1234harsh@cluster0.bsbrmty.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://harshsrivastava:manya1234harsh@cluster0.wjd38lz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
-app.get('/', (req,res) => {
-  res.send("hello")
-})
 
 async function run() {
   try {
@@ -33,11 +26,13 @@ async function run() {
       res.send(post);
     });
 
-    app.get('/userPost', async (req, res) => {
+    app.get("/userPost", async (req, res) => {
       const email = req.query.email;
-      const post = (await postCollection.find({ email: email }).toArray()).reverse();
+      const post = (
+        await postCollection.find({ email: email }).toArray()
+      ).reverse();
       res.send(post);
-  })
+    });
 
     app.post("/post", async (req, res) => {
       const post = req.body;
@@ -57,20 +52,23 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/userUpdates/:email', async (req, res) => {
+    app.patch("/userUpdates/:email", async (req, res) => {
       const filter = { email: req.params.email };
       const profile = req.body;
       const options = { upsert: true };
       const updateDoc = { $set: profile };
-  
+
       try {
-          const result = await userCollection.updateOne(filter, updateDoc, options);
-          res.send(result);
+        const result = await userCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
       } catch (error) {
-          res.status(500).send(error.message);
+        res.status(500).send(error.message);
       }
-  });  
-  
+    });
   } catch (error) {
     console.error("Error saving user:", error);
   }
